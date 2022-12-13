afee5755d845f7bc39fc7f46267f3665a97d8d2a
Muhammad farhan <mmfarhan120@gmail.com>
2022-12-13 16:28:22 +0500

    new feats

---
+++ lib/pages/feat post medicine request/in_progress_request.dart
@@ -0,0 +1,98 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:medicine_donation_app/widgets/medicine_request_card.dart';
+
+class InProgressRequest extends StatefulWidget {
+  const InProgressRequest({Key? key}) : super(key: key);
+
+  @override
+  State<InProgressRequest> createState() => _InProgressRequestState();
+}
+
+class _InProgressRequestState extends State<InProgressRequest> {
+  Stream medsStream =
+      FirebaseFirestore.instance.collection('medsreq').snapshots();
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: () async {
+        return false;
+      },
+      child: SafeArea(
+        child: GestureDetector(
+          onTap: () {
+            FocusScope.of(context).unfocus();
+          },
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: const [
+                    // Icon(
+                    //   Icons.chevron_right,
+                    //   size: 30,
+                    //   color: Color(0xff8C52FF),
+                    // ),
+                    Text(
+                      'Medicines Request List',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                Divider(
+                  height: 1.5,
+                  color: Colors.grey.shade900,
+                ),
+                const SizedBox(height: 20),
+                Expanded(
+                  child: MedReqCard(medsStream),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+Widget MedReqCard(medsStream) {
+  return StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+    stream: medsStream,
+    builder: (BuildContext context, snapshot) {
+      if (snapshot.hasError) {
+        print('Something went Wrong');
+      }
+      if (snapshot.data == null) {
+        print('yo');
+      }
+      if (snapshot.connectionState == ConnectionState.waiting) {
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      }
+
+      return ListView.builder(
+        itemCount: snapshot.data?.docs.length,
+        itemBuilder: (context, index) {
+          final DocumentSnapshot documentSnap = snapshot.data!.docs[index];
+
+          return (
+            MedicineRequestCard(
+            medicineName: documentSnap['medname'].toString(),
+            medicineQuantity: documentSnap['quant'].toString(),
+            address: documentSnap['address'].toString(),
+            userName: documentSnap['name'].toString(),
+            contactNumber: documentSnap['phone'].toString(),
+          ));
+        },
+      );
+    },
+  );
+}
